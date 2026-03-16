b9bd14865df47475f03a0d854485274644a5ffd4
Tulio Cunha <tuliopinheiroc23@gmail.com>
2026-03-16 09:32:12 -0300

    config

---
+++ test-github.ts
@@ -0,0 +1,27 @@
+import { loadQuery } from "./src/sanity/lib/load-query.ts";
+import { getMergedGitHubData } from "./src/lib/github-data.ts";
+
+async function test() {
+  console.log("Fetching Sanity repos...");
+  const query = `
+    *[_type == "featuredGithubRepo" && featured == true && visibleInProofOfWork == true] | order(order asc) {
+      _id,
+      repoFullName,
+      displayTitle,
+      description,
+      category,
+      featured,
+      order,
+      showRepositoryLink,
+      showPrivate,
+      visibleInProofOfWork
+    }
+  `;
+  try {
+    const { data } = await loadQuery({ query });
+    console.log("Sanity repos:", data);
+  } catch (err) {
+    console.error("Sanity query error:", err);
+  }
+}
+test();
